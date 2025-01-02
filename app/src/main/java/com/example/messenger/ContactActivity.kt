e368f98d561b1540efd4fcbea96ed34d83dbca52
camillafalk <ccamilla.falk@gmail.com>
2025-01-02 13:53:16 +0100

    RecyclerView for contacts works and is scrollable.

--- app/src/main/java/com/example/messenger/ContactActivity.kt
+++ app/src/main/java/com/example/messenger/ContactActivity.kt
@@ -5,26 +5,45 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.google.firebase.firestore.toObject
 
 
 class ContactActivity() : AppCompatActivity() {
+   var contacts = mutableListOf<User>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        lateinit var contacts : List<User>
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-//test
-        var recyclerView = findViewById<RecyclerView>(R.id.chatLists)
-        val adapter = MessagesAdapter(this, contacts)
-
-
         setContentView(R.layout.activity_messenger_overview)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        var db = Firebase.firestore
+        var recyclerView = findViewById<RecyclerView>(R.id.chatLists)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = ContactRecycleAdapter(this, contacts)
         recyclerView.adapter = adapter
+
+        val docRef = db.collection("Users")
+
+        docRef.get().addOnSuccessListener { documentSnapShot ->
+
+            for (document in documentSnapShot.documents) {
+
+                val user = document.toObject<User>()
+
+                if (user != null) {
+                    contacts.add(user)
+                }
+            }
+            adapter.notifyDataSetChanged()
+        }
     }
 }
