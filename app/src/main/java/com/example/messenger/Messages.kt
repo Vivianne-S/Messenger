180721a0b44d4abd77d72382c129657e47ea4391
camillafalk <ccamilla.falk@gmail.com>
2025-01-03 12:26:53 +0100

    Starting to add messages. Not done!

--- app/src/main/java/com/example/messenger/Messages.kt
+++ app/src/main/java/com/example/messenger/Messages.kt
@@ -1,6 +1,6 @@
 package com.example.messenger
 
-class Messages  (val userId : String, val contactUser : String, val message : String, val timeStamp : String){
+class Messages  (val userId : String, val message : String, val timeStamp : String){
 
 }
 
