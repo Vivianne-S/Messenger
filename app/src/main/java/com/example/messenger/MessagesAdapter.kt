d2866eee751d31ee02f3d9169bac64f26cf170e9
DJmoos3 <isaac.strandh@gmail.com>
2025-01-04 15:19:35 +0100

    Fixed so the background of the messages change color depending on who sent it.

--- app/src/main/java/com/example/messenger/MessagesAdapter.kt
+++ app/src/main/java/com/example/messenger/MessagesAdapter.kt
@@ -1,11 +1,17 @@
 package com.example.messenger
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.annotation.ContentView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 
 class MessagesAdapter(
     context: Context,
@@ -13,6 +19,7 @@ class MessagesAdapter(
 ) : RecyclerView.Adapter<MessagesAdapter.MessageViewHolder>() {
 
     private val layoutInflater = LayoutInflater.from(context)
+    lateinit var cardView : CardView
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
         val itemView = layoutInflater.inflate(R.layout.message_item, parent, false)
@@ -21,13 +28,20 @@ class MessagesAdapter(
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         val message = messages[position]
+        val currentUsedId = Firebase.auth.currentUser?.uid
+
+        cardView = holder.itemView.findViewById(R.id.cardView)
+
+        if(message.userId == currentUsedId) {
+            cardView.setCardBackgroundColor(Color.parseColor("#e75555"))
+        }
 
         //TODO should show who sent, the receivers name...
-       holder.userTextView.text = message.email
+        holder.userTextView.text = message.email
+
 
         //Show the sent message.
-       holder.messageTextView.text = message.message
-
+        holder.messageTextView.text = message.message
     }
 
     override fun getItemCount(): Int {
