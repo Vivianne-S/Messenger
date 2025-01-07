6bd8f4cc3c16fd300642b569d20c086d95dad506
DJmoos3 <isaac.strandh@gmail.com>
2025-01-07 09:30:22 +0100

    colors got removed on a merge so fixed them again

--- app/src/main/java/com/example/messenger/MessagesAdapter.kt
+++ app/src/main/java/com/example/messenger/MessagesAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.messenger
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import org.w3c.dom.Text
 
 class MessagesAdapter(
@@ -14,6 +18,7 @@ class MessagesAdapter(
 ) : RecyclerView.Adapter<MessagesAdapter.MessageViewHolder>() {
 
     private val layoutInflater = LayoutInflater.from(context)
+    lateinit var cardView : CardView
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
         val itemView = layoutInflater.inflate(R.layout.message_item, parent, false)
@@ -22,9 +27,21 @@ class MessagesAdapter(
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         val message = messages[position]
+        val currentUsedId = Firebase.auth.currentUser?.uid
+
        holder.userTextView.text = message.email
        holder.messageTextView.text = message.message
         holder.timeStampTV.text = message.timeStamp
+
+
+        //gives access to the cardview
+        cardView = holder.itemView.findViewById(R.id.cardView)
+
+        //this changes the color of the user
+        //only need to reverse to change the other users color
+        if(message.userId == currentUsedId) {
+            cardView.setCardBackgroundColor(Color.parseColor("#e75555"))
+        }
 
     }
 
